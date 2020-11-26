523a8c32bdc2700404664804426fa97305f9ead9
0003c9fe <20267593+0003c9fe@users.noreply.github.com>
2020-11-26 14:20:14 +1100

    Finished 2015 Day 04

--- 2015/Day 04 - The Ideal Stocking Stuffer/4.py
+++ 2015/Day 04 - The Ideal Stocking Stuffer/4.py
@@ -1,1 +1,20 @@
+import hashlib
 input = "iwrupvqb"
+
+found = False
+answer = 1
+while not found:
+    hashInput = input + str(answer)
+    found = (hashlib.md5(hashInput.encode()).hexdigest()[0:5] == "00000")
+    answer += 1
+
+print("The lowest positive number producing a hash with 5 leading zeroes is",answer-1)
+
+found = False
+answer = 1
+while not found:
+    hashInput = input + str(answer)
+    found = (hashlib.md5(hashInput.encode()).hexdigest()[0:6] == "000000")
+    answer += 1
+
+print("The lowest positive number producing a hash with 6 leading zeroes is",answer-1)
