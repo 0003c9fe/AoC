f4c5e91cc12cbd2ba84cee1a4cd3fc09c3173c96
0003c9fe <20267593+0003c9fe@users.noreply.github.com>
2020-11-28 15:49:46 +1100

    Finished 2016 Day 03

---
+++ 2016/Day 03 - Squares With Three Sides/3.py
@@ -0,0 +1,33 @@
+with open('input.txt') as file:
+    input = [line.rstrip() for line in file]
+
+def valid(a, b, c):
+    return (a + b > c and a + c > b and b + c > a)
+
+possible = 0
+for triangle in input:
+    sides = list(map(int,triangle.split()))
+    if valid(sides[0], sides[1], sides[2]):
+        possible += 1
+
+print("Considering rows,",possible,"of the listed triangles are possible")
+
+column1 = []
+column2 = []
+column3 = []
+for triangle in input:
+    sides = list(map(int,triangle.split()))
+    column1.append(sides[0])
+    column2.append(sides[1])
+    column3.append(sides[2])
+
+possible = 0
+columns = [column1, column2, column3]
+for column in columns:
+    i = 0
+    while i < len(column):
+        if valid(column[i], column[i+1], column[i+2]):
+            possible += 1
+        i += 3
+
+print("Considering columns",possible,"of the listed triangles are possible")
