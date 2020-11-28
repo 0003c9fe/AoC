ef98e9f4deacf527c32a21f371ee1992cbb9767d
0003c9fe <20267593+0003c9fe@users.noreply.github.com>
2020-11-29 01:02:53 +1100

    Finished 2016 Day 04

--- 2016/Day 04 - Security Through Obscurity/4.py
+++ 2016/Day 04 - Security Through Obscurity/4.py
@@ -19,7 +19,29 @@ for room in input:
 
     if checkSum(letters) == checksum:
         idSum += sectorID
-        realRooms.append(room)
+        realRooms.append((' '.join(room.split('-')[0:-1]),sectorID))
 
 print("The sum of the sector IDs of the real rooms is",idSum)
-print(realRooms)
+
+realNames = []
+offset = ord('a')
+#('amjmpdsj cee qcptgacq', 236)
+for room in realRooms:
+    words = room[0]
+    id = room[1]
+    translation = ''
+    for letter in words:
+        if letter == ' ':
+            translation += ' '
+        else:
+            translation += chr((((ord(letter)-offset)+id) % 26)+offset)
+    realNames.append((translation,id))
+
+found = False
+i = -1
+while not found:
+    i += 1
+    found = realNames[i][0] == 'northpole object storage'
+
+poleID = realNames[i][1]
+print("The sector ID of the room where North Pole objects are stored is",poleID)
