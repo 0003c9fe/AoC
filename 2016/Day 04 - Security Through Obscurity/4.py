8dea98d700c3dd191bde8b95a344b94018408c04
0003c9fe <20267593+0003c9fe@users.noreply.github.com>
2020-11-29 00:40:37 +1100

    Work on 2016 - Day 04

--- 2016/Day 04 - Security Through Obscurity/4.py
+++ 2016/Day 04 - Security Through Obscurity/4.py
@@ -1,8 +1,25 @@
+from collections import Counter
 with open('input.txt') as file:
     input = [line.rstrip() for line in file]
 
-test = input[0]
-print(test.replace('-','',test.count('-')-1).split('-'))
+def checkSum(letters):
+    freq = Counter(letters).most_common(5)
+    checksum = ''
+    for pair in freq:
+        checksum += pair[0]
+    return checksum
 
+idSum = 0
+realRooms = []
+for room in input:
+    filtered = room.replace('-','',room.count('-')-1).replace('[','-[').split('-')
+    letters = sorted(filtered[0])
+    sectorID = int(filtered[1])
+    checksum = filtered[2].strip('[]')
 
-#for room in input:
+    if checkSum(letters) == checksum:
+        idSum += sectorID
+        realRooms.append(room)
+
+print("The sum of the sector IDs of the real rooms is",idSum)
+print(realRooms)
