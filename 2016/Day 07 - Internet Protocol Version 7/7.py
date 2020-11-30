a1bbe704797780e43bb0f8e80f0aa2453348c2c0
0003c9fe <20267593+0003c9fe@users.noreply.github.com>
2020-11-30 20:29:09 +1100

    Removed unnecessary files

--- 2016/Day 07 - Internet Protocol Version 7/7.py
+++
@@ -1,9 +0,0 @@
-import re
-with open('input.txt') as file:
-    input = [line.rstrip() for line in file]
-
-string1 = 'xxx'
-string2 = 'xyx'
-
-aba = re.compile(r"(.)(?!\1)\1")
-print(re.search(aba,string2))
